2d6f25ab6673ad72255d9bba4c6060c95ffc7f83
rrdblx <61373148+rrdblx@users.noreply.github.com>
2021-04-20 13:38:46 +0800

    注册和登录页样式再次修改

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,25 @@
 <template>
-    <div class="login">
-        <div class="background">
-            <img :src="imgSrc" alt="" width="100%" height="100%">
+    <div id="login">
+        <div class="backgl">
+            <img :src="imgSrc" alt="" >
         </div>
         <div class="loginBox">
             <div class="header">
-                <h1 class="zh">欢迎登录xxx</h1>
+                <h1 class="log">欢迎登录xxx</h1>
             </div>
             <div v-if="type==2">
-            <mt-field style="background:transparent;padding-bottom:10px;color:white" class="inp" type="text" label="手机号码" placeholder="请输入您的手机号" :state="phoneState" v-model="phone" @blur.native.capture="checkPhone"></mt-field>
-            <mt-field style="background:transparent;padding-bottom:10px;color:white" class="inp" type="password" label="短信验证码" placeholder="请输入短信验证码" v-model="message" :state="messageState" @blur.native.capture="checkMessage">
-                <mt-button class="bu"   size="small" ><router-link class="hq" to="" @click="sendCode">获取验证码</router-link></mt-button>
+            <mt-field style="background:transparent;padding-bottom:10px;color:white" id="inp" type="text" label="手机号码" placeholder="请输入您的手机号" :state="phoneState" v-model="phone" @blur.native.capture="checkPhone"></mt-field>
+            <mt-field style="background:transparent;padding-bottom:10px;color:white" id="inp" type="password" label="短信验证码" placeholder="请输入短信验证码" v-model="message" :state="messageState" @blur.native.capture="checkMessage">
+                <mt-button class="btn1"   size="small" ><router-link class="sendCode1" to="" @click="sendCode">获取验证码</router-link></mt-button>
             </mt-field>
             </div>
             <div v-if="type==2">
-            <mt-field style="background:transparent;padding-bottom:10px;color:white" class="inp" type="text" label="用户名" placeholder="请输入用户名" :state="usernameState" v-model="username" @blur.native.capture="checkUsername"></mt-field>
-            <mt-field style="background:transparent;padding-bottom:10px;color:white" class="inp" type="password" label="密码" placeholder="请输入密码" v-model="pwd" :state="pwdState" @blur.native.capture="checkPwd"></mt-field>
+            <mt-field style="background:transparent;padding-bottom:10px;color:white" id="inp" type="text" label="用户名" placeholder="请输入用户名" :state="usernameState" v-model="username" @blur.native.capture="checkUsername"></mt-field>
+            <mt-field style="background:transparent;padding-bottom:10px;color:white" id="inp" type="password" label="密码" placeholder="请输入密码" v-model="pwd" :state="pwdState" @blur.native.capture="checkPwd"></mt-field>
             </div>
             <mt-button class="dl" type="primary" size="large" @click="checkForm">快速登录</mt-button>
-            <router-link to="" class="changeBtn">用户密码登录</router-link>
-           <router-link to="" class="changeBtn">短信验证码登录</router-link>
+            <router-link to="" class="changeBtn1">用户密码登录</router-link>
+           <router-link to="" class="changeBtn1">短信验证码登录</router-link>
             <router-link to="/register" class="changezc">立即注册</router-link>
         </div>
     </div>
@@ -41,6 +41,7 @@ export default {
         }
     },
     methods:{
+
         checkUsername(){
             let username=this.username;
             
@@ -91,6 +92,7 @@ export default {
             }else{
                 
             }
+
         },
         //发送验证码
         sendCode(){
@@ -102,22 +104,25 @@ export default {
     }
 }
 </script>
-<style>
-.login .background{
+<style >
+    .backgl{
         width: 100%;
         height: 100%;
         z-index: -1;
         position: fixed;
     }
-.login .inp{
-    background:transparent;padding-bottom:10px;
-}
-.login .dl{
-    margin:0 auto;width:200px;
-    margin-bottom: 20px !important;
-}
-.login .bu{
-        background-color: gray !important;
+    .log{
+        padding-top: 100px;
+        font-size: 30px;
+        color:white;
+        padding-bottom:10px;
+    }
+    .loginBox{
+        z-index: 1;
+    }
+
+
+    .btn1{
         opacity: 0.6;
         
     }
@@ -125,12 +130,21 @@ export default {
         margin-bottom: 20px !important;
         margin:0 auto;width:200px !important;
     }
-    .changeBtn{
+    .changeBtn1{
         color:white;
        
     }
     .changezc{
         color:white;
         margin-left: 20px;
+        
+    }
+    input::placeholder{
+    color:white !important;
+    
+    }
+    input{background: transparent !important;}
+    .sendCode1{
+        color: black;
     }
 </style>
