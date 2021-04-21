a9f3d43af83a9a59021595919d6f08f271c12657
Lucinel <82713230+Lucinel@users.noreply.github.com>
2021-04-21 21:56:31 +0800

    接口完成 端口为3000

--- server/app.js
+++ server/app.js
@@ -9,22 +9,22 @@ const bodyParse = require('body-parser');
 // 创建Mysql连接池
 //需要修改
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST, //MySQL服务器地址
-    port: process.env.MYSQL_PORT, //MySQL服务器端口号
-    user: process.env.ACCESSKEY, //数据库用户的用户名
-    password : process.env.SECRETKEY,
-    database : 'app_' + process.env. APPNAME, //数据库用户密码
-    // database: 'app_teamwork', //数据库名称
-    connectionLimit: 20, //最大连接数
-    charset: 'utf8' //数据库服务器的编码方式
-
-    // host: '127.0.0.1',   //MySQL服务器地址
-    // port: '3306',          //MySQL服务器端口号
-    // user: 'root',        //数据库用户的用户名
-    // password: '',        //数据库用户密码
-    // database: 'newclear_pollution',    //数据库名称
+    // host: process.env.MYSQL_HOST, //MySQL服务器地址
+    // port: process.env.MYSQL_PORT, //MySQL服务器端口号
+    // user: process.env.ACCESSKEY, //数据库用户的用户名
+    // password : process.env.SECRETKEY,
+    // database : 'app_' + process.env. APPNAME, //数据库用户密码
+    // // database: 'app_teamwork', //数据库名称
     // connectionLimit: 20, //最大连接数
-    // charset: 'utf8'      //数据库服务器的编码方式
+    // charset: 'utf8' //数据库服务器的编码方式
+
+    host: '127.0.0.1',   //MySQL服务器地址
+    port: '3306',          //MySQL服务器端口号
+    user: 'root',        //数据库用户的用户名
+    password: '',        //数据库用户密码
+    database: 'newclear_pollution',    //数据库名称
+    connectionLimit: 20, //最大连接数
+    charset: 'utf8'      //数据库服务器的编码方式
   });
 //   创建服务器对象
 const server = express();
@@ -68,7 +68,71 @@ server.get('/category', (req, res) => {
   // 返回数据到客户端
   res.send({ message: 'ok', code: 2000, result: results[0] });
   });
-  
+  //author页面接口
+  server.get('/author', (req, res) => {
+    let author_id=req.query.author_id
+    // SQL语句以获取文章分类表的数据
+    let sql = 'SELECT author_name,author_avatar FROM np_author where author_id=?';
+    // 执行SQL语句
+    pool.query(sql, [author_id],(error, results) => {
+    if (error) throw error;
+    res.send({ message: 'authot is ok', code: 200, results: results });
+      });
+    });
+
+    // 
+    //评论表接口
+    server.get('/comment', (req, res) => {
+      var news_id=req.query.news_id
+      // SQL语句以获取文章分类表的数据
+      let sql = 'SELECT comment_content,comment_id,user_id FROM np_news_comment where news_id=?';
+      // 执行SQL语句
+      pool.query(sql,[news_id],(error, results) => {
+      if (error) throw error;
+      res.send({ message: 'comment is ok', code: 200, results: results });
+        });
+      });    
+    
+    //用户注册接口
+    server.post('/register', (req, res) => {
+      //console.log(md5('12345678'));
+      // 获取用户名和密码信息
+      let uname = req.body.user_name;
+      let upwd = req.body.upwd;
+      //以username为条件进行查找操作，以保证用户名的唯一性
+      let sql = 'SELECT COUNT(id) AS count FROM xzqa_author WHERE username=?';
+      pool.query(sql, [uname], (error, results) => {
+        if (error) throw error;
+        let count = results[0].count;
+        if (count == 0) {
+          // 将用户的相关信息插入到数据表
+          sql = 'INSERT np_user(user_id,upwd) VALUES(?,MD5(?))';
+          pool.query(sql, [user_id, upwd], (error, results) => {
+            if (error) throw error;
+            res.send({ message: 'ok', code: 200 });
+          })
+        } else {
+          res.send({ message: 'user exists', code: 201 });
+        }
+      });
+    });
+
+    // 用户登录接口
+    server.post('/login', (req, res) => {
+      //获取用户名和密码信息
+      let uname = req.body.user_name;
+      let upwd = req.body.upwd;
+      // SQL语句
+      let sql = 'SELECT user_id,phone,avatar FROM np_user WHERE uname=? AND pwd=MD5(?)';
+      pool.query(sql, [uname, upwd], (error, results) => {
+        if (error) throw error;
+        if(results.length == 0){ //登录失败
+          res.send({message:'login failed',code:201});
+        } else {                 //登录成功
+          res.send({message:'ok',code:200,result:results[0]});
+        }
+      });
+    });
   });
 //
 // *****************************************************************
