1bad8b66acf837d8e37d4103935dff0730a1fdcf
Liqinghan-99 <78136507+Liqinghan-99@users.noreply.github.com>
2021-04-22 22:02:20 +0800

    增加了详情页面的具体内容

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -3,6 +3,7 @@
     <header>
       <!-- 头部图片位置 暂时用div代替-->
       <div style="width：100%;height:160px; background-color:red;"></div>
+      <!-- <img :src="detail.image" style="width：100%;height:160px;" alt=""> -->
     </header>
     <!-- 头部结束 -->
     <!--正文开始  -->
@@ -10,22 +11,21 @@
       <!-- 正文导航 -->
       <div class="nav clearfix">
         <div class="nav_img">
+          <!-- 作者头像 待修改 -->
           <img src="../assets/logo.png" alt class="n_img">
         </div>
-        <span class="nav_s">新浪/刚刚</span>
+        <span class="nav_s">{{detail.created_at}}</span>
         <!-- <span class="nav_a">新浪/刚刚</span> -->
         <router-link to="#" class="nav_a ">已订阅</router-link>
       </div>
       <!-- 导航结束 -->
       <!-- 正文标题 -->
       <div class="headline">
-        <h2>核污染主要指核物质泄露后的遗留物对环境的破坏</h2>
+        <h2>{{detail.title}}</h2>
       </div>
       <!-- 内容信息 -->
       <div class="content">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem commodi temporibus doloribus consequatur cupiditate cumque culpa, quidem, veniam tenetur aliquid ad modi. Earum soluta natus, officia ullam sapiente dolorum quidem!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem commodi temporibus doloribus consequatur cupiditate cumque culpa, quidem, veniam tenetur aliquid ad modi. Earum soluta natus, officia ullam sapiente dolorum quidem!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem commodi temporibus doloribus consequatur cupiditate cumque culpa, quidem, veniam tenetur aliquid ad modi. Earum soluta natus, officia ullam sapiente dolorum quidem!</p>
+        <p>{{detail.content}}</p>
       </div>
       <div>
         <!-- <button>赞</button> -->
@@ -69,12 +69,17 @@ export default {
       // 声明URL为接口地址
       let url = `/detail?news_id=${id}`
       console.log(url)
-      this.axios.get(url).then(results=>{
+      this.axios.get(url).then(result=>{
         //打印从服务器端返回的数据
-        console.log(results);
+        console.log(result);
 
         //把服务器返回的文章对象存入data
-        // this.detail = result.data.result
+        this.detail = result.data.results
+
+         // 处理一下时间  通过毫秒值时间戳  获取moment对象
+            let m = this.moment(this.detail.created_at*1000);
+            // 新增属性createdStr用于保存格式化后的时间字符串
+            this.detail.createdStr = m.format('YYYY年MM月DD日 HH:mm');
       })
       
     }
@@ -83,7 +88,7 @@ export default {
 
 
 
-<style>
+<style scoped>
 /* 导航样式 */
 .nav {
   margin-top: 10px;
