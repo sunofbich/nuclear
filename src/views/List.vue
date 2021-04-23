15106a81c2b82e5ed31d8ffe932ed1f20effd4ec
zhaovi <78091828+zhaovi@users.noreply.github.com>
2021-04-23 23:36:22 +0800

    添加了获得菜单栏的接口

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,31 +1,38 @@
 <template>
   <div>
     <!-- 导航 -->
-    <mt-navbar v-model="navactive" fixed>
-      <mt-tab-item id="1">推荐</mt-tab-item>
-      <mt-tab-item id="2">国内</mt-tab-item>
-      <mt-tab-item id="3">国外</mt-tab-item>
-      <mt-tab-item id="4">科普</mt-tab-item>
+    <mt-navbar style="top: 40px" v-model="navactive" fixed>
+      <mt-tab-item v-for="(item, i) in category" :key="i" :id="item.id + ''">{{
+        item.category_name
+      }}</mt-tab-item>
     </mt-navbar>
 
     <!-- 中部面板 -->
     <mt-tab-container v-model="navactive">
       <mt-tab-container-item id="1">
-        <h1>面板1</h1>
-        <h1>面板1</h1>
-        <h1>面板1</h1>
-        <h1>面板1</h1>
-        <h1>面板1</h1>
-        <h1>面板1</h1>
-        <h1>面板1</h1>
-        <h1>面板1</h1>
-        <h1>面板1</h1>
-        <h1>面板1</h1>
-        <h1>面板1</h1>
+        <!-- 单一文章信息开始 -->
+        <div class="articleItem" v-for="(item, i) in articles" :key="i">
+          <!-- 文章标题开始 -->
+          <div class="articleItem-header">{{ item.subject }}</div>
+          <!-- 文章标题结束 -->
+          <!-- 文章图文信息开始 -->
+          <div class="articleItem-wrapper">
+            <!-- 文章图像开始  -->
+            <div class="articleImg">
+              <!-- <img :src="`../assets/articles/${item.image}`" /> -->
+              <!-- <img src="../assets/articles/4-methods-to-make-competitive-interactive-works-b.jpg" /> -->
+              <img v-if="item.image" :src="item.image" />
+            </div>
+            <!-- 文章图像结束 -->
+            <!-- 文章简介开始 -->
+            <div class="articleDes">
+              {{ item.description }}
+            </div>
+            <!-- 文章简介结束 -->
+          </div>
+          <!-- 文章图文信息结束 -->
+        </div>
       </mt-tab-container-item>
-      <mt-tab-container-item id="2">2</mt-tab-container-item>
-      <mt-tab-container-item id="3">3</mt-tab-container-item>
-      <mt-tab-container-item id="4">4</mt-tab-container-item>
     </mt-tab-container>
 
     <!-- 底部选项卡 -->
@@ -57,23 +64,25 @@ export default {
     return {
       navactive: "1",
       selected: "home",
+      category: [],
+      articles: [],
     };
   },
-  mounted(){
-    //  *******待使用*******
-    //  // 发送http请求，获取UI类别下的第一页数据
-    // this.loadArticles(1, 1, (list)=>{
-    //   // list即是发送请求后 响应中的文章列表
-    //   this.articles = list;
-    // })
+  mounted() {
+      // 发送http请求，获取当前新闻类别下的第一页数据
+    this.axios.post(`/list?category_name=推荐`).then(result=>{
+        console.log(result)
+        this.articles = result.data.results
+    })
 
-    //发送http请求,获取新闻类别的第一页数据
-    this.axios.get('/list').then(resule=>{
-      console.log(resule);
+    // 发送http请求，获取类别列表
+    //也就是头部菜单栏目
+    this.axios.get("/category").then((result) => {
+      console.log(result);
       // 把服务端返回的类别数组存入data中
       // result.data.results里存储着类别数组 [{},{},{},{}]
       this.category = result.data.results;
-    })
+    });
   },
   //监听底部选项卡,用于跳转主页以及me
   watch: {
@@ -83,8 +92,16 @@ export default {
       } else if (newval == "me") {
         this.$router.push("/me");
       }
-    }
+    },
   },
 
+  // 监听顶部导航的更新
+  navactive(newval) {
+    // newval即是当前选中项的id    类别id    cid
+    this.axios.get(`/list?category_name=${newval}`).then((result) => {
+      console.log(result);
+      this.articles = result.data.results;
+    });
+  },
 };
 </script>
