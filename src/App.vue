028b8bc99948d1eaa6a0d6f5ef45908e3c721609
zhaovi <78091828+zhaovi@users.noreply.github.com>
2021-04-25 21:18:00 +0800

    增加了顶部的导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <div id="app">
-    <router-view/>
+    <mt-header fixed title="核污染专题栏目" class="important-header">
+        <mt-button icon="back" @click="back" slot="left" ></mt-button>
+        <router-link to="/login">
+        <mt-button disabled  slot="right">登录</mt-button>
+
+        </router-link>
+    </mt-header>
+
+    <!-- 路由内容 -->
+    <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    back(){
+        this.$router.push(history.go(-1));
+    }
+  },
+}
+</script>
