2ef44fa3ad24fcd28ac27d99a17039cd02380790
zhaovi <78091828+zhaovi@users.noreply.github.com>
2021-04-25 21:19:28 +0800

    重新修改了新闻类别的获取方式

--- src/views/List.vue
+++ src/views/List.vue
@@ -2,9 +2,12 @@
   <div class="list">
     <!-- 导航 -->
     <mt-navbar style="top: 0" v-model="navactive" fixed>
-      <mt-tab-item v-for="(item, i) in category" :key="i" :id="item.category_id + ''">{{
-        item.category_name
-      }}</mt-tab-item>
+      <mt-tab-item
+        v-for="(item, i) in category"
+        :key="i"
+        :id="item.category_id + ''"
+        >{{ item.category_name }}</mt-tab-item
+      >
     </mt-navbar>
 
     <!-- 中部面板 -->
@@ -28,7 +31,7 @@
               <div class="articleImg">
                 <!-- <img :src="`../assets/articles/${item.image}`" /> -->
                 <!-- <img src="../assets/articles/4-methods-to-make-competitive-interactive-works-b.jpg" /> -->
-               <!-- 图片没有   <img v-if="item.image" :src="item.image" /> -->
+                <!-- 图片没有   <img v-if="item.image" :src="item.image" /> -->
               </div>
               <!-- 文章图像结束 -->
               <!-- 文章简介开始 -->
@@ -45,26 +48,6 @@
         <div v-if="reachBottom" class="rb">我是有底线的</div>
       </mt-tab-container-item>
     </mt-tab-container>
-
-    <!-- 底部选项卡 -->
-    <mt-tabbar v-model="selected" fixed>
-      <mt-tab-item id="home">
-        <img
-          v-if="selected == 'home'"
-          src="../assets/home/main_1.png"
-          slot="icon"
-        />
-        <img v-else src="../assets/home/main_0.png" slot="icon" />
-      </mt-tab-item>
-      <mt-tab-item id="me">
-        <img
-          v-if="selected == 'me'"
-          src="../assets/home/me_1.png"
-          slot="icon"
-        />
-        <img v-else src="../assets/home/me_0.png" slot="icon" />
-      </mt-tab-item>
-    </mt-tabbar>
   </div>
 </template>
 
@@ -74,7 +57,7 @@
 export default {
   data() {
     return {
-      navactive: "1",
+      navactive: "10",
       selected: "home",
       category: [],
       articles: [],
@@ -114,12 +97,11 @@ export default {
     },
   },
   mounted() {
-   // 发送http请求，获取当前新闻类别下的第一页数据
+    // 发送http请求，获取当前新闻类别下的第一页数据
     this.axios.get(`/list?category_id=10`).then((result) => {
-      console.log(result);
+      // console.log(result);
       this.articles = result.data.results;
     });
-
     // 发送http请求，获取类别列表
     //也就是头部菜单栏目
     this.axios.get("/category").then((result) => {
@@ -129,24 +111,17 @@ export default {
       this.category = result.data.results;
     });
   },
-  //监听底部选项卡,用于跳转主页以及me
   watch: {
-    selected(newval) {
-      if (newval == "home") {
-        this.$router.push("/list");
-      } else if (newval == "me") {
-        this.$router.push("/me");
-      }
-    },
-  },
-
-  // 监听顶部导航的更新
-  navactive(newval) {
-    // 把page变量重置为1 
+    // 监听顶部导航的更新
+    navactive(newval) {
+      // 把page变量重置为1
+      console.log(newval);
       this.page = 1;
-      this.loadArticles(newval, 1, (list)=>{
-        this.articles = list;
-    });
+      this.axios.get(`list?category_id=${newval}`).then((result) => {
+        console.log(result);
+        this.articles = result.data.results;
+      });
+    },
   },
 };
 </script>
