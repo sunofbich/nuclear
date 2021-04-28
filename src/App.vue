e0611acca644ea005ec0adec43a6d8719750e284
zhaovi <78091828+zhaovi@users.noreply.github.com>
2021-04-28 21:30:40 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,60 @@
   <div id="app">
     <mt-header fixed title="核污染专题栏目" class="important-header">
         <mt-button icon="back" @click="back" slot="left" ></mt-button>
-        <router-link to="/login">
-        <mt-button disabled  slot="right">登录</mt-button>
-
-        </router-link>
+        <router-link v-if="this.$store.state.islogin==0" to="/login" slot="right">登录/注册</router-link>
+        <router-link v-else slot="right" to="/me">{{this.$store.state.user.user_name}}<span>注销</span></router-link>
     </mt-header>
 
     <!-- 路由内容 -->
     <router-view />
+    <!-- 底部选项卡开始 -->
+      <mt-tabbar v-model="selected" fixed>
+              <mt-tab-item id="home">
+                  <img
+            v-if="selected == 'home'"
+            src="./assets/home/main_1.png"
+            slot="icon"
+          />
+                  <img v-else src="./assets/home/main_0.png" slot="icon" />
+                </mt-tab-item
+        >
+              <mt-tab-item id="me">
+                  <img
+            v-if="selected == 'me'"
+            src="./assets/home/me_1.png"
+            slot="icon"
+          />
+                  <img v-else src="./assets/home/me_0.png" slot="icon" />
+                </mt-tab-item
+        >
+            </mt-tabbar
+      >
+      <!-- 底部选项卡结束 -->
   </div>
 </template>
 
+
+
 <script>
 export default {
+data(){
+	return{
+	selected: "home",
+	}
+},
   methods: {
-    back(){
+	back(){
         this.$router.push(history.go(-1));
     }
+  },
+  watch: {
+    selected(newval) {
+      if (newval == "home") {
+        this.$router.push("/");
+      } else if (newval == "me") {
+        this.$router.push("/me");
+      }
+    },
   },
 }
 </script>
